e3688d22d6705789bb591ae4aa44a7208ce1ba93
KeadenM <keadenmeacham@gmail.com>
2023-07-16 12:31:26 -0600

    finished city search javascript and added weatherapp API

--- Assests/script.js
+++ Assests/script.js
@@ -22,4 +22,33 @@ function saveCity(cityName) {
     function loadPreviousSearches() {
         var cities = JSON.parse(localStorage.getItem('cities')) || [];
         previousSearchesEl.innerHTML = '';
-    }
+
+        cities.forEach(function (city) {
+            var cityBtn = document.createElement('button');
+            cityBtn.textContent = city;
+            cityBtn.classList.add('previous-search-btn');
+            cityBtn.addEventListener('click', function () {
+                weatherData(city);
+                
+            });
+            previousSearchesEl.appendChild(cityBtn);
+            });
+        }
+
+        var citySubmitHandler = function (event) {
+            event.preventDefault();
+          
+            var cityName = capitalizeFirstLetter(cityInputEl.value.trim());
+            console.log(cityName);
+            if (cityName) {
+            weatherData(cityName);
+            cityInputEl.value = '';
+            } else {
+              alert('Please enter a city');
+            }
+            };
+
+            function weatherData(cityName){
+                var apiKey = 'd94fcd0a3f247519e9f2462c13c0bc86';
+                var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+            }
