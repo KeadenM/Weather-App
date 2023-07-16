828e9ed1903d34cba654591ac5972fd404d0473d
KeadenM <keadenmeacham@gmail.com>
2023-07-16 14:55:04 -0600

    needed to generate new key so new one added

--- Assests/script.js
+++ Assests/script.js
@@ -49,6 +49,30 @@ function saveCity(cityName) {
             };
 
             function weatherData(cityName){
-                var apiKey = 'd94fcd0a3f247519e9f2462c13c0bc86';
+                var apiKey = '3d3ea1abe7ae02576c706c0260592e5c';
                 var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
-            }
+                fetch(weatherUrl)
+                .then(function (response) {
+                if (response.status === 200) { 
+                    saveCity(cityName);
+                        loadPreviousSearches();
+                return response.json();
+                    } else {
+                alert('Failed to fetch weather data');
+                }
+                })
+                .then(function (data) {
+                var timestamp = data.dt;
+                var date = new Date(timestamp * 1000);
+                var todaysDate = date.toISOString().split('T')[0];
+                var temperature = data.main.temp;
+                var wind = data.wind.speed;
+                var humidity = data.main.humidity;
+                    console.log(temperature, wind, humidity);
+                displayWeatherData(todaysDate, cityName, temperature, wind, humidity);
+                    weeklyData(cityName);
+                })
+                    .catch(function (error) {
+                alert('Error fetching weather data');
+                });
+                };
